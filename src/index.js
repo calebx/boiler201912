3a2910df3662d447baca731ef8c82c7c25ef7f56
caleb.xiang <caleb.xiang@sinobot.com.cn>
2019-12-02 11:16:31 +0800

    add blank html partial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import "./index.scss";
 
-function component() {
-    const elem = document.createElement("h1");
-    elem.innerHTML = "hola, yoyo.";
-    return elem;
-}
-
-document.body.appendChild(component());
 console.log(["hi", "nothing"].join(", "));
