92edf9a0f4bc3758f4830c3a8b5319cf66f1df60
caleb.xiang <caleb.xiang@sinobot.com.cn>
2019-12-02 09:18:58 +0800

    babel-runtime is better

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ function component() {
 
 document.body.appendChild(component());
 console.log(_.join(["hi", "nothing"]));
+
+var sym = Symbol();
+
+var promise = Promise.resolve();
+
+var check = arr.includes("yeah!");
+
+console.log(arr[Symbol.iterator]());
